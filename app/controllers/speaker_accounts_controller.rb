338f91789d88211dcaa7551f4825eedccda3700f
mnmsnrk <69619645+mnmsnrk@users.noreply.github.com>
2020-10-20 15:02:06 +0900

    マイページのview実装

--- app/controllers/speaker_accounts_controller.rb
+++ app/controllers/speaker_accounts_controller.rb
@@ -1,6 +1,10 @@
 class SpeakerAccountsController < ApplicationController
   before_action :authenticate_speaker!
 
+  # def index
+  #   @speaker = Speaker.all
+  # end
+
   def show
     @speaker = Speaker.find(params[:id])
   end
